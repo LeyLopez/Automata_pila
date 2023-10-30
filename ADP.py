cef27197cb81b9679dfd275555a2914851f480c9
LeyLopez <leidislopez2017@gmail.com>
2023-10-30 00:19:39 -0500

    Changes

--- ADP.py
+++ ADP.py
@@ -1,35 +1,15 @@
 from Pila import Stack 
 
 class AutomataDePila:
-    def __init__(self, status, input_alphabet, stack_alphabet, transitions, initial_status, acceptance_status):
+    def __init__(self, status, input_alphabet, stack_alphabet, transitions, initial_status, acceptance_status, position):
         self.status = status
         self.input_alphabet = input_alphabet
         self.stack_alphabet = stack_alphabet
         self.transitions = transitions
         self.initial_status = initial_status
         self.acceptance_status = acceptance_status
+        self.position = position
         
-    
-    def process_chain(self, chain):
-        stack = Stack()
-        current_status = self.initial_status
-        
-        for simbol in chain:
-            if(current_status, simbol, stack.top()) in self.transitions:
-                new_status, simbols_to_stack = self.transitions[(current_status, simbol, stack.top())]
-                current_status = new_status
-                
-                if simbols_to_stack != '':
-                    for simbol_to_stack in simbols_to_stack[::-1]:
-                        if simbol_to_stack != '':
-                            stack.push(simbol_to_stack)
-                        else:
-                            stack.pop()
-                        
-            else:
-                return False
-        
-        return current_status == self.acceptance_status
     
     def get_status(self):
         return self.status
@@ -48,3 +28,7 @@ class AutomataDePila:
     
     def get_acceptance_status(self):
         return self.acceptance_status
+    
+    
+    def get_position(self):
+        return self.position
--- main.py
+++ main.py
@@ -1,79 +1,52 @@
-import tkinter as tk
-from Pila import Stack
+from ADP import AutomataDePila
+from GUI import Interface
+from PySide6.QtWidgets import QApplication
 
-class Automaton:
-    def __init__(self):
-        self.stack = Stack()
-        self.states = {
-            'q0': {'a': ('q1', 'pop_push'), 'b': ('q3', 'push')},
-            'q1': {'a': ('q1', 'pop_push'), 'b': ('q2', 'pop')},
-            'q2': {'a': ('q2', 'pop'), 'b': ('q2', 'pop')},
-            'q3': {'a': ('q3', 'push'), 'b': ('q4', 'push')},
-            'q4': {'a': ('q4', 'push'), 'b': ('q2', 'pop')}
-        }
-        self.current_state = 'q0'
+import sys
 
-    def reset(self):
-        self.stack = Stack()
-        self.current_state = 'q0'
+def iniciar():
 
-    def process_input(self, input_str):
-        self.reset()
-        for symbol in input_str:
-            if symbol in self.states[self.current_state]:
-                next_state, action = self.states[self.current_state][symbol]
-                if action == 'pop_push':
-                    self.stack.pop()
-                elif action == 'pop':
-                    self.stack.pop()
-                elif action == 'push':
-                    self.stack.push(symbol)
-                self.current_state = next_state
-                yield (self.current_state, symbol)
+    automata = create_automata()
+    aplicacion = QApplication(sys.argv)
+    root = Interface(automata)
+    root.show()
+    sys.exit(aplicacion.exec())
+    
+def create_automata():
+    
+    estados = {'p', 'q', 'r'}
+    alfabeto_entrada = {'a', 'b', ''}
+    alfabeto_pila = {'a', 'b', '#'}
+    
+    transiciones = {
+        ('p', 'b', 'b'): ('p', 'bb'),
+        ('p', 'a', 'b'): ('p', 'ba'),
+        ('p', 'b', 'a'): ('p', 'ab'),
+        ('p', 'a', 'a'): ('p', 'aa'),
+        ('p', 'b', '#'): ('p', '#b'),
+        ('p', 'a', '#'): ('p', '#a'),
+        ('p', 'b', 'b'): ('q', ''),
+        ('p', 'a', 'a'): ('q', ''),
+        ('q', 'b', 'b'): ('q', ''),
+        ('q', 'a', 'a'): ('q', ''),
+        ('q', '', '#'): ('r', '#')
+    }
+    estado_inicial = {'p'}
+    estado_aceptacion ={'r'}
+    
+    posicion = {
+        "p": (1, 0),
+        "q": (1, 1),
+        "r": (2, 2)
+    }
+    
+    automata = AutomataDePila(estados, alfabeto_entrada, alfabeto_pila, transiciones, estado_inicial, estado_aceptacion, posicion)
+    return automata
+    
+    
 
-    def is_valid(self):
-        return self.current_state == 'q2' and self.stack.is_empty()
-
-class PalindromeRecognizerApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title('Palindrome Recognizer')
-        self.root.geometry("600x400")
-        
-        self.label = tk.Label(root, text='Enter a string (abbbba format):')
-        self.label.pack()
-
-        self.entry = tk.Entry(root)
-        self.entry.pack()
-
-        self.check_button = tk.Button(root, text='Check', command=self.check_palindrome)
-        self.check_button.pack()
-
-        self.result_label = tk.Label(root, text='Result: ')
-        self.result_label.pack()
-
-        self.state_label = tk.Label(root, text='Current State: q0')
-        self.state_label.pack()
-
-        label_style = ("Helvetica", 14, "bold")  # Cambiar la fuente, el tamaño y la negrita
-
-        self.label.config(fg="Purple", font=label_style)  # Cambiar el color y el estilo
-        self.result_label.config(fg="Purple", font=label_style)  # Cambiar el color y el estilo
-        self.state_label.config(fg="Purple", font=label_style)  # Cambiar el color y el estilo
-
-    def check_palindrome(self):
-        input_str = self.entry.get()
-        automaton = Automaton()
-        for state, symbol in automaton.process_input(input_str):
-            self.state_label.config(text=f'Current State: {state} -> Next Symbol: {symbol}')
-            self.root.update()
-        
-        if automaton.is_valid():
-            self.result_label.config(text='Result: Valid Palindrome')
-        else:
-            self.result_label.config(text='Result: Not a valid palindrome')
-
-if __name__ == '__main__':
-    root = tk.Tk()
-    app = PalindromeRecognizerApp(root)
-    root.mainloop()
+    
+iniciar()
+    
+    
+    
