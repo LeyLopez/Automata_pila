b4b55bc73825cc3e6e160c45de0aef7393eb8370
LeyLopez <leidislopez2017@gmail.com>
2023-10-29 19:59:54 -0500

    GUI

--- myi18nlibrary/__init__.py
+++
@@ -1,7 +0,0 @@
-import gettext
-import os
-
-def configure_i18n(domain, locale_dir):
-    gettext.bindtextdomain(domain, locale_dir)
-    gettext.textdomain(domain)
-    return gettext.gettext
--- myi18nlibrary/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name = "myi18nlibrary",
-    version = "1.0",
-    packages = find_packages(),
-)
